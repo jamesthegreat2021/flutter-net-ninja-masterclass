dbb98f700a357f3173dbd62640dbfafde91df8ac
jamesthegreat2021 <jamesmemba2000@gmail.com>
2025-07-01 17:52:36 +0300

    abstracted the costom widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'quote.dart';
+import 'quote_list.dart';
 
 void main() {
   runApp(MaterialApp(
@@ -37,42 +38,3 @@ class _QuoteListState extends State<QuoteList> {
   }
 }
 
-class QuoteWidget extends StatelessWidget {
-  const QuoteWidget({
-    super.key,
-    required this.quote,
-  });
-
-  final dynamic quote;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Card(
-        margin: EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              quote.name,
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.grey[600]
-              ),
-            ), 
-            SizedBox(height: 6.0),
-            Text(
-              quote.author, 
-              style: TextStyle(
-                fontSize: 14.0,
-                color: Colors.grey[800],
-              ),
-      
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
